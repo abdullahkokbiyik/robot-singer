d137c7b7ff9246498eeb8b359fa514c88a940028
mfurkanyavuz <m.furkanyavuz96@gmail.com>
2019-12-01 19:28:39 +0300

    updated script

--- running_script.py
+++ running_script.py
@@ -1,8 +1,12 @@
 import os
+from generate_abc_file import generate_abc_file
 
+file_name = input("Give your abc file a name: \n")
+generate_abc_file(file_name)
 for file in os.listdir("./example"):
     if file.endswith(".abc"):
         print(file[:-4].capitalize())
 option = input("Type the name of abc file listed above: \n")
+
 
 os.system("cd ecantorix && make ../example/" + option.lower() + ".wav")
